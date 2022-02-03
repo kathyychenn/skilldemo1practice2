b0e7112e0a038fb884327e3f273f4084506367bc
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-02-03 14:29:59 -0800

    create SkillDemo.java and SkillDemoTest.java and lib

---
+++ SkillDemo.java
@@ -0,0 +1,5 @@
+public class SkillDemo{
+    public static String birthday(int age){
+        return "happy " + age + "th birthday";
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * Commands to run in terminal:
+ * javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar SkillDemoTest.java
+ * java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore SkillDemoTest
+ */
+
+public class SkillDemoTest {
+    @Test
+    public void test1(){
+        assertEquals("happy 49th birthday", SkillDemo.birthday(50));
+    }
+}
